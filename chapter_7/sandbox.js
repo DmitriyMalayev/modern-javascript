f67be5cda5876fa4400f2a5de1c3980d49d841d4
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-09-19 02:23:50 -0400

    regex

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,9 +1,32 @@
-const form = document.querySelector('.signup-form');
+const form = document.querySelector(".signup-form");
+const feedback = document.querySelector(".feedback");
 // const username = document.querySelector('#username');
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //prevents the default action of refreshing the page
   // console.log('form submitted');
-  // console.log(username.value);
+  // console.log(username.value) references the attribute (id or name) via dot notation;
   console.log(form.username.value);
 });
+
+const username = "dmitriy";
+const pattern = /^[a-z]{6,}$/;
+let result = pattern.test(username); //return boolean response
+console.log(result);
+let results = pattern.test();
+
+if (results) {
+  console.log("regex test passed");
+} else {
+  console.log("regex test failed");
+}
+
+if (usernamePattern.test(username)) {
+  feedback.textContent = "That username is valid";
+} else {
+  feedback.textContent =
+    "This username is invalid, it can only includes letters and be at least 6 characters long.";
+}
+
+let pattern_result = username.search(pattern);
+//returns an integer with the index position of where the match has been found.
